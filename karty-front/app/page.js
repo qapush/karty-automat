1dacd802bd14bc593a8d5d0caf2f409ca840aea5
qapush <mihail.kapush@gmail.com>
2025-02-22 16:38:27 +0100

    prefetch false for index page links

--- karty-front/app/page.js
+++ karty-front/app/page.js
@@ -1,26 +1,36 @@
-import Link from "next/link";
-
+import Link from 'next/link';
 
 export default function Home() {
-  return <div style={{maxWidth: 800, margin: 'auto', marginTop: '2rem'}}>
-    <h2>Dekoracje bez ID</h2>
-      <ul style={{ marginTop: '2rem'}}>
+  return (
+    <div style={{ maxWidth: 800, margin: 'auto', marginTop: '2rem' }}>
+      <h2>Dekoracje bez ID</h2>
+      <ul style={{ marginTop: '2rem' }}>
         <li>
-            <Link href='/dekoracje/28'>ZM</Link>
+          <Link prefetch={false} href="/dekoracje/28">
+            ZM
+          </Link>
         </li>
         <li>
-            <Link href='/dekoracje/69'>CC</Link>
+          <Link prefetch={false} href="/dekoracje/69">
+            CC
+          </Link>
         </li>
         <li>
-            <Link href='/dekoracje/41'>MQ</Link>
+          <Link prefetch={false} href="/dekoracje/41">
+            MQ
+          </Link>
         </li>
         <li>
-            <Link href='/dekoracje/42'>PM</Link>
+          <Link prefetch={false} href="/dekoracje/42">
+            PM
+          </Link>
         </li>
         <li>
-            <Link href='/dekoracje/11'>JS</Link>
+          <Link prefetch={false} href="/dekoracje/11">
+            JS
+          </Link>
         </li>
       </ul>
-   
-  </div>;
+    </div>
+  );
 }
